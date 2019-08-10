a5690cc6c79e5f6c6d2b419724d2383638ed098d
David Dryburgh <david.dryburgh1994@gmail.com>
2019-08-11 00:32:59 +0100

    udpated to return ride result set

--- src/main/java/com/pluralsight/repository/RideRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/RideRepositoryImpl.java
@@ -1,10 +1,13 @@
 package com.pluralsight.repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import com.pluralsight.model.Ride;
@@ -17,18 +20,24 @@ public class RideRepositoryImpl implements RideRepository {
 	
 	@Override
 	public List<Ride> getRides() {
-		Ride ride = new Ride();
-		ride.setName("Corner Canyon");
-		ride.setDuration(120);
-		List <Ride> rides = new ArrayList<>();
-		rides.add(ride);
+		List<Ride> rides = jdbcTemplate.query("SELECT * FROM ride", new RowMapper<Ride> () {
+
+			@Override
+			public Ride mapRow(ResultSet rs, int rowNum) throws SQLException {
+				Ride ride = new Ride();
+				ride.setId(rs.getInt("id"));
+				ride.setName(rs.getString("name"));
+				ride.setDuration(rs.getInt("duration"));
+				return ride;
+			}
+			
+		});
 		return rides;
 	}
 
 	@Override
 	public Ride createRide(Ride ride) {
-		jdbcTemplate.update("insert into ride (name, duration) values (?,?)", ride.getName(), ride.getDuration());
-		
+		jdbcTemplate.update("insert into ride (name, duration) values (?,?)", ride.getName(), ride.getDuration());		
 		return null;
 	}
 	
