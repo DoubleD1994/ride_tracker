50c9da8142c1a62228037c5a3a42edb7d96af008
David Dryburgh <david.dryburgh1994@gmail.com>
2019-08-11 00:55:23 +0100

    modified to use annonymous inner classes

--- src/main/java/com/pluralsight/repository/RideRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/RideRepositoryImpl.java
@@ -1,16 +1,19 @@
 package com.pluralsight.repository;
 
-import java.sql.ResultSet;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import com.pluralsight.model.Ride;
+import com.pluralsight.repository.util.RideRowMapper;
 
 @Repository("rideRepository")
 public class RideRepositoryImpl implements RideRepository {
@@ -20,25 +23,30 @@ public class RideRepositoryImpl implements RideRepository {
 	
 	@Override
 	public List<Ride> getRides() {
-		List<Ride> rides = jdbcTemplate.query("SELECT * FROM ride", new RowMapper<Ride> () {
-
-			@Override
-			public Ride mapRow(ResultSet rs, int rowNum) throws SQLException {
-				Ride ride = new Ride();
-				ride.setId(rs.getInt("id"));
-				ride.setName(rs.getString("name"));
-				ride.setDuration(rs.getInt("duration"));
-				return ride;
-			}
-			
-		});
+		List<Ride> rides = jdbcTemplate.query("SELECT * FROM ride", new RideRowMapper());
 		return rides;
 	}
 
 	@Override
 	public Ride createRide(Ride ride) {
-		jdbcTemplate.update("insert into ride (name, duration) values (?,?)", ride.getName(), ride.getDuration());		
-		return null;
+		KeyHolder keyHolder = new GeneratedKeyHolder();
+		jdbcTemplate.update(new PreparedStatementCreator() {
+			
+			@Override
+			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+				PreparedStatement ps = con.prepareStatement("INSERT INTO ride (name, duration) VALUES (?,?)", new String [] {"id"});
+				ps.setString(1, ride.getName());
+				ps.setInt(2, ride.getDuration());
+				return ps;
+			}
+		}, keyHolder);
+		Number id = keyHolder.getKey();
+		return getRide(id.intValue());
+	}
+	
+	private Ride getRide(Integer id) {
+		Ride ride = jdbcTemplate.queryForObject("SELECT * FROM ride WHERE id = ?", new RideRowMapper(), id);
+		return ride;
 	}
 	
 }
