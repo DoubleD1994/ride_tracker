9e7780e5fb082c873ab4ed49ff7ba54f4e4b0473
David Dryburgh <david.dryburgh1994@gmail.com>
2019-08-11 02:16:23 +0100

    added exception handling

--- src/test/java/com/pluralsight/controller/RestControllerTest.java
+++ src/test/java/com/pluralsight/controller/RestControllerTest.java
@@ -70,4 +70,10 @@ public class RestControllerTest {
 		RestTemplate restTemplate = new RestTemplate();
 		restTemplate.delete("http://localhost:8080/ride_tracker/delete/1");
 	}
+	
+	@Test(timeout=3000)
+	public void testException() {
+		RestTemplate restTemplate = new RestTemplate();
+		restTemplate.getForObject("http://localhost:8080/ride_tracker/test", Ride.class);
+	}
 }
